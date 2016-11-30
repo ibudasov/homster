7fc87faa166b99afb885431a156d006603699457
Igor <igor.budasov@insided.com>
2016-11-30 18:46:37 +0100

    Removing back compartybility with apex

--- updateRemoteDependencies.sh
+++ updateRemoteDependencies.sh
@@ -5,18 +5,14 @@
 
 declare PROJECT_DIRECTORY="/Users/ibudasov/Sites/homster"
 
-# symlink to proper index file to have back compartibility with Apex
-ln ${PROJECT_DIRECTORY}/functions/change/index.js  ${PROJECT_DIRECTORY}/index.js
-
 # making ZIP archive
-echo Packing...
+echo Deploying...
 zip -r homster_change.zip . > /dev/null
 
 # and, actually, deploy
 aws lambda update-function-code --function-name homster_change --zip-file fileb://${PROJECT_DIRECTORY}/homster_change.zip > /dev/null
 
-echo Deployed.
+echo OK
 
 # let's call it clean up
-rm ${PROJECT_DIRECTORY}/index.js
 rm ${PROJECT_DIRECTORY}/homster_change.zip
