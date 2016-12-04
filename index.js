7d0a5416e42c98c9a4a1a9757d3cce37e388d9d8
Igor <igor.budasov@insided.com>
2016-12-04 17:22:28 +0100

    Get method draft

--- index.js
+++ index.js
@@ -6,15 +6,17 @@ let AWS = require('aws-sdk');
 let uuid = require('node-uuid');
 let db = new AWS.DynamoDB.DocumentClient({region: 'eu-west-1'});
 let tableName = 'homster';
-let done = (err, res) => callback(null, {
-    statusCode: err ? '400' : '200',
-    body: err ? err.message : JSON.stringify(res),
-    headers: {
-        'Content-Type': 'application/json',
-    },
-});
 
 exports.handle = (event, context, callback) => {
+
+    let done = (err, res) => callback(null, {
+        statusCode: err ? '400' : '200',
+        body: err ? err.message : JSON.stringify(res),
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+
     switch (event.httpMethod) {
         case 'GET':
             changeController.get(event, context, callback);
@@ -28,34 +30,51 @@ exports.handle = (event, context, callback) => {
     }
 };
 
+
+let changeObject = {
+    id: uuid.v1(),
+    timestampReceived: Date.now().toString(),
+    deviceType: 'qwertyui',
+    userId: 'werty',
+    payload: {
+        qwert: 'qwert'
+    }
+};
+
 let changeController = {
 
-    get: function (event, context, callback) {
-        // db.get({
+    /**
+     * Returns changes between two dates -- timestampFrom and timestampTo.
+     * These timestamps are required, because otherwise response will be too big
+     *
+     * @param event
+     * @param context
+     * @param response
+     */
+    get: function (event, context, response) {
 
-        let dateFrom = 0;
-        let dateTo = 0;
+        let timestampFrom = guardTimestampFrom(event, response);
+        let timestampTo = guardTimestampTo(event, response);
 
         db.scan({
             Limit: 100,
-            TableName: tableName
+            TableName: tableName,
+            ScanFilter: {
+                "timestampReceived": {
+                    AttributeValueList: [timestampFrom],
+                    ComparisonOperator: "GT"
+                }
+            }
         }, function (err, data) {
             if (err) {
-                callback(err, null);
+                response(err, null);
             } else {
-                callback(null, data);
+                response(null, data);
             }
         })
     },
 
     post: function () {
-
-        let changeObject = {
-            id: uuid.v1(),
-            deviceType: "undefined",
-            userId: "someString",
-            timeCreated: Date.now().toString()
-        };
 
         db.put({
             Item: changeObject,
@@ -68,5 +87,22 @@ let changeController = {
             }
         })
 
+    },
+
+};
+
+let guardTimestampFrom = function (event, callback) {
+    if (event.timestampFrom.length !== 0) {
+        return event.timestampFrom;
+    } else {
+        callback('timestampFrom is required', null);
+    }
+};
+
+let guardTimestampTo = function (event, callback) {
+    if (event.timestampTo.length !== 0) {
+        return event.timestampTo;
+    } else {
+        callback('timestampTo is required', null);
     }
 };
