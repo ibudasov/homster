926aa471137ffb7e4b249a9da120851b0fa70124
Igor <igor.budasov@insided.com>
2016-12-04 17:22:55 +0100

    Small improvement for deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,16 +3,13 @@
 # It's possible to deploy AWS Lambda with dependencies only by uploading ZIP file.
 # Sure, someone can do it manually, but here's my script. It's not perfect, but still work.
 
-declare PROJECT_DIRECTORY="/Users/ibudasov/Sites/homster"
+declare PROJECT_DIRECTORY="."
 
 # making ZIP archive
-echo Deploying...
 zip -r homster_change.zip . > /dev/null
 
 # and, actually, deploy
 aws lambda update-function-code --function-name homster_change --zip-file fileb://${PROJECT_DIRECTORY}/homster_change.zip > /dev/null
-
-echo OK
 
 # let's call it clean up
 rm ${PROJECT_DIRECTORY}/homster_change.zip
